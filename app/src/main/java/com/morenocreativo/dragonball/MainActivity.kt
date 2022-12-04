370c0c19c8b2de26ee160ce889c5c9107e3f6157
MorenoCreativo <adrielmoreno@hotmail.com>
2022-12-04 16:15:46 +0100

    Ui login created

--- app/src/main/java/com/morenocreativo/dragonball/MainActivity.kt
+++ app/src/main/java/com/morenocreativo/dragonball/MainActivity.kt
@@ -2,10 +2,14 @@ package com.morenocreativo.dragonball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_constraint)
+        Log.v(MainActivity::class.java.name, "Prueba")
+        var hola = "hola"
+        println(hola)
     }
 }
